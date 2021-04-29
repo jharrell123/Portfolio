e30dba38ea78a640d2bb1a058d65878967e96a22
jharrell123 <jharrell@uccs.edu>
2021-04-29 19:03:31 +0000

    More Simplecov stuff

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Visiting the homepage", type: :feature do
+RSpec.feature "HomePages", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
     expect(page).to have_text("Projects")
